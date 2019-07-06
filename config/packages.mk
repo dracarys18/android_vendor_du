45c2daca9cee8163e29e8ee29c41623ea60c8cd0
dracarys18 <karthihegde010@gmail.com>
2019-07-06 13:27:22 +0000

    Build Updater
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -2,7 +2,8 @@
 PRODUCT_PACKAGES += \
     CustomDoze \
     WallpaperPicker \
-    WeatherClient
+    WeatherClient \
+    Updater
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
